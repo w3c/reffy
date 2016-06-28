787e626a79370662a2454be42e605fcac16c2764
François Daoust <fd@tidoust.net>
2016-06-28 10:49:09 +0200

    Bikeshed references parsing: also return informative references
    Structure returned now looks like:
    
    {
      "normative": [],
      "informative": []
    }
    
    Note parsing reports an error when there are no normative references or no
    informative references. We may want to be more flexible later on and report
    warnings instead: specs may in theory not reference anything.

--- parse-references.js
+++ parse-references.js
@@ -16,23 +16,37 @@ function extract(url, cb) {
 }
 
 function extractBikeshedReferences(doc, cb) {
-    var refHeading = doc.getElementById('normative');
-    if (!refHeading) {
-        cb(new Error("Spec " + url + " is generated with bikeshed but does not have a 'normative' id"));
-    }
-    var referenceList = doc.querySelector("#normative + dl");
-    if (!referenceList) {
-        cb(new Error("Spec " + url + " is generated with bikeshed but does not have a definition list following the heading with id 'normative'"));
-    }
-    var references = [];
-    [].forEach.call(referenceList.querySelectorAll("dt"), function(dt) {
-        var ref = {};
-        ref.name = dt.textContent.replace(/[\[\] \n]/g, '');
-        var desc = dt.nextSibling;
-        ref.url = desc.querySelector("a[href]").href;
-        references.push(ref);
+    var extractReferencesFromList = function(referenceList) {
+        return [].map.call(referenceList.querySelectorAll("dt"), function(dt) {
+            var ref = {};
+            ref.name = dt.textContent.replace(/[\[\] \n]/g, '');
+            var desc = dt.nextSibling;
+            ref.url = desc.querySelector("a[href]").href;
+            return ref;
+        });
+    };
+
+    var error = null;
+    var references = {};
+    ['normative', 'informative'].forEach(function(referenceType) {
+        if (error) return;
+        var refHeading = doc.getElementById(referenceType);
+        if (!refHeading) {
+            error = new Error("Spec " + url + " is generated with bikeshed but does not have a '" + referenceType  + "' id");
+        }
+        var referenceList = doc.querySelector("#" + referenceType + " + dl");
+        if (!referenceList) {
+            error = new Error("Spec " + url + " is generated with bikeshed but does not have a definition list following the heading with id '" + referenceType + "'");
+        }
+        references[referenceType] = extractReferencesFromList(referenceList);
     });
-    cb(null, references);
+
+    if (error) {
+        return cb(error);
+    }
+    else {
+        cb(null, references);
+    }
 }
 
 module.exports.extract = extract;
@@ -48,6 +62,6 @@ if (require.main === module) {
             console.error(err);
             process.exit(64);
         }
-        console.log(JSON.stringify(references));
+        console.log(JSON.stringify(references, null, 2));
     });
 }
