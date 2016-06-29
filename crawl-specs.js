3f1109e36802fe0ab9cb9d6a23c72b63d611d5d4
Dominique Hazael-Massieux <dom@w3.org>
2016-06-29 17:06:43 +0200

    Crawl editors draft when available
    This uses the w3c API, so needs a key configured in a config.json file with key 'w3cApiKey'

--- crawl-specs.js
+++ crawl-specs.js
@@ -1,6 +1,37 @@
 var refParser = require('./parse-references');
 var webidlExtractor = require('./extract-webidl');
 var webidlParser = require('./parse-webidl');
+var fetch = require('node-fetch');
+
+function getShortname(url) {
+    if (!url.match(/www.w3.org\/TR\//)) {
+        return url;
+    }
+    if (url.match(/TR\/[0-9]+\//)) {
+        // dated version
+        var statusShortname = url.split('/')[5];
+        return statusShortname.split('-').slice(1, -1).join('-');
+    }
+    // latest version
+    return url.split('/')[4];
+}
+
+function getLatest(shortname) {
+    var config = require("./config.json");
+    var authParam = '?apikey=' + config.w3cApiKey;
+    if (shortname.match(/^http/)) {
+        return shortname;
+    }
+    return fetch('https://api.w3.org/specifications/' + shortname + '' + authParam)
+        .then(r =>  r.json())
+        .then(s => fetch(s._links["latest-version"].href + authParam))
+        .then(r =>  r.json())
+        .then(s => s["editor-draft"] ? s["editor-draft"] : s.uri);
+}
+
+function latestSpec(list) {
+    return Promise.all(list.map(getShortname).map(getLatest));
+}
 
 function crawlList(speclist) {
     function getRefAndIdl(url) {
@@ -36,7 +67,8 @@ if (require.main === module) {
         console.error("Impossible to read " + speclistPath + ": " + e);
         process.exit(3);
     }
-    crawlList(speclist)
+    latestSpec(speclist)
+        .then(crawlList)
         .then(function (data) {
             console.log(JSON.stringify(data, null, 2));
         })
