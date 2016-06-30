14ea95804758b4b2f7c2e9799c2183a818577214
François Daoust <fd@tidoust.net>
2016-06-30 17:36:59 +0200

    Improved study report layout

--- study-specs.js
+++ study-specs.js
@@ -2,12 +2,59 @@ var array_concat = (a,b) => a.concat(b);
 var array_unique = (n, i, a) => a.indexOf(n) === i;
 
 function processResults(results) {
-    results.filter(r => !r.refs.normative || r.refs.normative.length === 0).forEach(r => console.error(r.url + " has no normative dependency"));
-    results.filter(r => Object.keys(r.idl).length === 0 || !r.idl.idlNames || (Object.keys(r.idl.idlNames).length === 1 && Object.keys(r.idl.idlExtendedNames).length === 0)).forEach(r => console.error(r.url + " has no IDL"));
-    var idlNames = results.map(r =>  r.idl && r.idl.idlNames ? Object.keys(r.idl.idlNames).filter(n => n !== "_dependencies") : [], []).reduce(array_concat).filter(array_unique);
-    var idlDeps = results.map(r =>  r.idl && r.idl.externalDependencies ? r.idl.externalDependencies : [], []).reduce(array_concat).filter(array_unique);
+    var count = 0;
+    var write = console.log.bind(console);
+    write('List of specifications without normative dependencies');
+    write('=====================================================');
+    results
+        .filter(r => (!r.refs.normative || (r.refs.normative.length === 0)))
+        .forEach(r => {
+            count += 1;
+            write(r.url)
+        });
+    write();
+    write('=> ' + count + ' specification' + ((count > 1) ? 's' : '') + ' found');
+    write();
+    write('NB: it may be normal!');
+    write();
+    write();
+
+    count = 0;
+    write('List of specifications without IDL definitions')
+    write('==============================================');
+    results
+        .filter(r => ((Object.keys(r.idl).length === 0) ||
+            !r.idl.idlNames ||
+            ((Object.keys(r.idl.idlNames).length === 1) && (Object.keys(r.idl.idlExtendedNames).length === 0))))
+        .forEach(r => {
+            count += 1;
+            write(r.url)
+        });
+    write();
+    write('=> ' + count + ' specification' + ((count > 1) ? 's' : '') + ' found');
+    write();
+    write('NB: specifications that invalid IDL are included in that list!');
+    write();
+
+    count = 0;
+    write('List of IDL names not defined in the specifications crawled');
+    write('===========================================================');
+    var idlNames = results
+        .map(r => r.idl && r.idl.idlNames ? Object.keys(r.idl.idlNames).filter(n => n !== "_dependencies") : [], [])
+        .reduce(array_concat)
+        .filter(array_unique);
+    var idlDeps = results
+        .map(r => r.idl && r.idl.externalDependencies ? r.idl.externalDependencies : [], [])
+        .reduce(array_concat)
+        .filter(array_unique);
     var diff = idlDeps.filter(n => idlNames.indexOf(n) === -1);
-    console.log(diff);
+    count = diff.length;
+    diff.forEach(idlName => write(idlName));
+    write();
+    write('=> ' + count + ' IDL name' + ((count > 1) ? 's' : '') + ' found');
+    write();
+    write('NB: some of them are likely type errors in specs');
+    write('(e.g. "int" does not exist, "Array" cannot be used on its own, etc.)');
 }
 
 /**************************************************
