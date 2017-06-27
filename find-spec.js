cff055b411fbf93f457b4539f5a8ef0d9aa3d9aa
François Daoust <fd@tidoust.net>
2017-06-27 12:01:34 +0200

    Re-write find-spec as module, make report a param
    The path to the current crawl report should now be passed as parameter
    (not a good idea to have it hardcoded given that this report is not the Git
    repository).
    
    If needed, find-spec could now be used from another Node.js file.
    
    Also, this commit slightly improves the readability of the code.

--- find-spec.js
+++ find-spec.js
@@ -1,7 +1,12 @@
-const report = require("./results.json");
 const blacklist = require("./blacklist.json");
 const {JSDOM} = require("jsdom");
 
+/**
+ * Canonicalize a URL extracted from a page for comparison purpose
+ *
+ * @function
+ * @private
+ */
 const canonicalize = url => url
       .split('#')[0]
       .replace('http:', 'https:')
@@ -9,32 +14,78 @@ const canonicalize = url => url
       .replace('Overview.html', '')
       .replace(/https:\/\/([^\.]*).spec.whatwg.org\/.*/, 'https://$1.spec.whatwg.org/');
 
-const currentSpecs = report.map(s => s.versions.map(v => canonicalize(v)))
-      // flatten
-      .reduce((a,b) => a.concat(b), []);
 
-// List to which we compare
-// known specs, and ones we want to ignore
-const matchingSpecs = currentSpecs.concat(blacklist);
-
-
-const extractLinks = (url, selector) => new Promise((resolve, reject) => {
-    return JSDOM.fromURL(url).then(dom => {
-        const links = [...dom.window.document.querySelectorAll(selector)]
-              .map(a => canonicalize(a.href));
-        resolve(links.filter((u,i) =>
-                             // not in our list
-                             matchingSpecs.indexOf(u) === -1
-                             // and first link to there
-                             && links.indexOf(u) === i));
-    });
-});
+/**
+ * Retrieve the document at the specified location and extract links that
+ * match the given DOM selector.
+ *
+ * @function
+ * @private
+ */
+const extractLinks = (url, selector) =>
+    JSDOM.fromURL(url).then(dom =>
+        [...dom.window.document.querySelectorAll(selector)]
+            .map(a => canonicalize(a.href)));
 
 
-Promise.all(
-    [extractLinks("https://platform.html5.org/", "#contentCols dd a:first-child"),
-     extractLinks("https://www.w3.org/standards/techs/js", "td h4 a:not([href*=NOTE])")])
-    .then(diffs => console.log(JSON.stringify(
-        diffs.reduce((a,b) => a.concat(b)).filter((u,i, a) => a.indexOf(u) === i)
-        , null, 2)));
+/**
+ * Filter a list of links to keep only new ones.
+ *
+ * The function also removes duplicates.
+ *
+ * @function
+ * @private
+ */
+const filterLinks = (list, known) =>
+    list.filter((u, i) =>
+        (known.indexOf(u) === -1) && // Not in the list of known specs
+        (list.indexOf(u) === i));    // and first time we see that new spec
 
+
+/**
+ * Look at external sites and extract URLs of specs that we do not currenly
+ * have in our report, and that we have no a priori reason to ignore.
+ */
+const findNewSpecs = (knownSpecs) => Promise.all([
+    extractLinks("https://platform.html5.org/", "#contentCols dd a:first-child"),
+    extractLinks("https://www.w3.org/standards/techs/js", "td h4 a:not([href*=NOTE])")
+])
+    .then(lists => lists.reduce((a, b) => a.concat(b)))
+    .then(list => filterLinks(list, knownSpecs))
+    .then(list => list.sort());
+
+
+/**************************************************
+Export the find method for use as module
+**************************************************/
+module.exports.finNewSpecs = findNewSpecs;
+
+
+/**************************************************
+Code run if the code is run as a stand-alone module
+**************************************************/
+if (require.main === module) {
+    let resultsPath = process.argv[2];
+    let report = [];
+    if (resultsPath) {
+        try {
+            report = require(resultsPath);
+        } catch(e) {
+            console.error("Impossible to read " + resultsPath + ": " + e);
+            process.exit(3);
+        }
+    }
+    else {
+        console.warn("No report given, proceeding with blacklist only");
+    }
+    let specsInReport = report
+        .map(s => s.versions.map(v => canonicalize(v)))
+        .reduce((a, b) => a.concat(b), []); // flatten
+
+    // Final list to which we want to compare URLs of specs we extract from
+    // external sites. That list includes specs we already know about, and
+    // specs we want to ignore
+    let knownSpecs = specsInReport.concat(blacklist);
+
+    findNewSpecs(knownSpecs).then(list => console.log(JSON.stringify(list, null, 2)));
+}
