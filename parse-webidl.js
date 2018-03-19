07e505a913b30c04b79c67449a1b2bec7749d4ad
Dominique Hazael-Massieux <dom@w3.org>
2018-03-19 11:03:44 +0100

    Includes mixin (#94)
    * Add support for interface mixin, close #92
    * Add support for includes, close #91

--- parse-webidl.js
+++ parse-webidl.js
@@ -133,6 +133,7 @@ function parseIdlAstTree(jsNames, idlNames, idlExtendedNames, externalDependenci
         switch(def.type) {
         case "namespace":
         case "interface":
+        case "interface mixin":
         case "dictionary":
         case "callback interface":
             parseInterfaceOrDictionary(def, jsNames, idlNames, idlExtendedNames, externalDependencies);
@@ -148,17 +149,18 @@ function parseIdlAstTree(jsNames, idlNames, idlExtendedNames, externalDependenci
         case "attribute":
         case "field":
             parseType(def.idlType, idlNames, externalDependencies, contextName);
-            break;
+          break;
+        case "includes":
         case "implements":
             parseType(def.target, idlNames, externalDependencies);
-            parseType(def.implements, idlNames, externalDependencies);
-            if (def.implements === 'window') {
+            parseType(def[def.type], idlNames, externalDependencies);
+            if (def[def.type] === 'window') {
                 idlNames._reallyDependsOnWindow = true;
             }
             if (!idlNames._dependencies[def.target]) {
                 idlNames._dependencies[def.target] = [];
             }
-            addDependency(def.implements, {}, idlNames._dependencies[def.target]);
+            addDependency(def[def.type], {}, idlNames._dependencies[def.target]);
             break;
         case "typedef":
             parseType(def.idlType, idlNames, externalDependencies);
