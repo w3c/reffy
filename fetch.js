9c5160cdbcd8a25157d648d5e4fe7109996ed837
François Daoust <fd@tidoust.net>
2018-05-15 12:17:20 +0200

    Have ReSpec run in JSDOM, stop using RespecDocWriter (#106)
    Reffy needs to run ReSpec on Editor's Drafts. It relied on RespecDocWriter
    until now, which uses Puppeeter, which in turn relies on a headless version
    of Chrome. That requires downloading 100s of MB of dependencies and is a tad
    heavy for the task at hands, especially as we consider running Reffy as an
    HTTP service.
    
    With this update, RespecDocWriter is no longer being used. Instead, Reffy makes
    ReSpec run in JSDOM, which is much more lightweight (and JS-based).
    
    Problem is that JSDOM does not yet support enough features to run ReSpec.
    Concrete issues are documented in:
    https://github.com/w3c/respec/issues/1469#issuecomment-388560878
    
    This update monkey-patches JSDOM's code to add good-enough implementations of
    missing features, as well as to regain control over network requests so that
    all network requests go through our cache. It also monkey-patches ReSpec to:
    
    1. drop a couple of modules that cannot run because they use non-implemented
    features of JSDOM that cannot be easily patched
    2. update the code of a couple of ReSpec dependencies for the same reason
    
    The whole thing remains fragile: updates to ReSpec or to its dependencies may
    well break the regular expressions used to monkey patch the code. Most of the
    monkey-patching should resist minor changes. A couple of them may not though.

---
+++ fetch.js
@@ -0,0 +1,48 @@
+/**
+ * Wrapper around the fetch module to setup a few config parameters from
+ * config.json
+ *
+ * @module finder
+ */
+
+const path = require('path');
+const baseFetch = require('fetch-filecache-for-crawling');
+
+// Read configuration parameters from `config.json` file
+let config = null;
+try {
+    config = require(path.resolve('config.json'));
+}
+catch (err) {
+    config = {};
+}
+
+
+/**
+ * Fetch function that applies fetch parameters defined in `config.json`
+ * unless parameters are already set.
+ *
+ * By default, force the HTTP refresh strategy to "once", so that only one
+ * HTTP request gets sent on a given URL per crawl.
+ *
+ * @function
+ * @param {String} url URL to fetch
+ * @param {Object} options Fetch options (and options for node-fetch, and
+ *   options for fetch-filecache-for-crawling)
+ * @return {Promise(Response)} Promise to get an HTTP response
+ */
+function fetch(url, options) {
+    options = Object.assign({}, options);
+    ['cacheFolder', 'resetCache', 'cacheRefresh', 'logToConsole'].forEach(param => {
+        let fetchParam = (param === 'cacheRefresh') ? 'refresh' : param;
+        if (config[param] && !options.hasOwnProperty(fetchParam)) {
+            options[fetchParam] = config[param];
+        }
+    });
+    if (!options.refresh) {
+        options.refresh = 'once';
+    }
+    return baseFetch(url, options);
+}
+
+module.exports = fetch;
