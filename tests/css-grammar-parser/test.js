fa36ea98ec25637af0d95d497185b908a3a344c4
Dominique Hazael-Massieux <dom@w3.org>
2018-07-09 16:29:24 +0200

    Add tests for CSS Property Definition values parser

---
+++ tests/css-grammar-parser/test.js
@@ -0,0 +1,13 @@
+const css = require("../../src/lib/css-grammar-parser");
+const fs = require("fs");
+const expect = require('chai').expect;
+
+const propDefs = fs.readFileSync("tests/css-grammar-parser/in", "utf-8").split("\n");
+const propDefsOut = JSON.parse(fs.readFileSync("tests/css-grammar-parser/out.json", "utf-8"));
+
+const results = propDefs.map(css.parsePropDefValue);
+for(let i in results) {
+  describe(`Parse property definition ${propDefs[i]} as expected`, () => {
+    expect(results[i]).to.deep.equal(propDefsOut[i], `Parsing ${propDefs[i]} got ${JSON.stringify(results[i], null, 2)} instead of ${JSON.stringify(propDefsOut[i], null, 2)}`);
+  });
+}
