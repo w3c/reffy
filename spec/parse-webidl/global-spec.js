49e4a1af88e3606e62ba931733fd824265309882
François Daoust <fd@w3.org>
2019-10-01 00:54:41 +0900

    WebIDL parsing updates to better support Global/Exposed/constructor (#186)
    Introduces the following changes:
    - Support for the new "constructor operation" syntax (#184). The old
    constructor syntax through extended attributes is still supported
    - Drop support for `[PrimaryGlobal]` and associated logic. That no longer exists
    in WebIDL (this change means that the /TR/ crawl may report a few additional
    hiccups, but so be it)
    - Expose defined globals in a `globals` property. They used to be reported
    under `idlNames` but they are neither JS names nor real IDL names, and this
    created false positives when specs reuse the same global name on purpose to
    inherit from other interfaces defined on it (for instance, the
    `AnimationWorkletGlobalScope` interface reuses `Worklet`)
    - Support for Global/Exposed attributes in partial definitions (#185)
    
    Also, the update flattens the IDL parts of the report and in particular
    drops `idlNames._dependencies` in favor of `dependencies`, and
    `idlNames._reallyDependsOnWindow` in favor or `reallyDependsOnWindow`. Resulting
    structure:
    
    ```json
    {
      jsNames: {
          constructors: {},
          functions: {}
      },
      idlNames: {},
      idlExtendedNames: {},
      globals: {},
      dependencies: {},
      externalDependencies: [],
      reallyDependsOnWindow: false
    }
    ```
    
    The update fixes the incorrect reports on IDL interfaces being defined in
    multiple specs (#115) as well.

---
+++ spec/parse-webidl/global-spec.js
@@ -0,0 +1,79 @@
+describe('The WebIDL parser understands Global/Exposed attributes', () => {
+  var parse = require('../../src/cli/parse-webidl').parse;
+
+  it('does not expose interface on Window by default', done => {
+    parse(`
+        interface notExposedOnWindow {};
+        `)
+      .then(data => {
+        expect(data).toHaveProperty('jsNames');
+        expect(data.jsNames).toHaveProperty('functions');
+        expect(data.jsNames.functions).not.toHaveProperty('Window');
+        expect(data.jsNames.functions.Window).not.toContain('notExposedOnWindow');
+      })
+      .catch(fail)
+      .then(done);
+  });
+
+  it('detects a simple global definition and references to it', done => {
+    parse(`
+        [Global]
+        interface primaryInterface {};
+
+        [Exposed=primaryInterface]
+        interface exposedOnPrimaryInterface {};
+        `)
+      .then(data => {
+        expect(data).toHaveProperty('globals');
+        expect(data.globals).toHaveProperty('primaryInterface');
+        expect(data.globals.primaryInterface).toContain('primaryInterface');
+        expect(data).toHaveProperty('jsNames');
+        expect(data.jsNames).toHaveProperty('functions');
+        expect(data.jsNames.functions).toHaveProperty('primaryInterface');
+        expect(data.jsNames.functions.primaryInterface).toContain('exposedOnPrimaryInterface');
+      })
+      .catch(fail)
+      .then(done);
+  });
+
+  it('uses the right name for a global interface definition', done => {
+    parse(`
+        [Global=theInterface, Exposed=theInterface]
+        interface anInterface {};
+        `)
+      .then(data => {
+        expect(data).toHaveProperty('globals');
+        expect(data.globals).toHaveProperty('theInterface');
+        expect(data.globals).not.toHaveProperty('anInterface');
+        expect(data.globals.theInterface).toContain('anInterface');
+        expect(data).toHaveProperty('jsNames');
+        expect(data.jsNames).toHaveProperty('functions');
+        expect(data.jsNames.functions).toHaveProperty('theInterface');
+        expect(data.jsNames.functions.theInterface).toContain('anInterface');
+      })
+      .catch(fail)
+      .then(done);
+  });
+
+  it('understands multiple names for a global interface definition', done => {
+    parse(`
+        [Global=(theInterface,sameInterface), Exposed=theInterface]
+        interface anInterface {};
+        `)
+      .then(data => {
+        expect(data).toHaveProperty('globals');
+        expect(data.globals).toHaveProperty('theInterface');
+        expect(data.globals).toHaveProperty('sameInterface');
+        expect(data.globals).not.toHaveProperty('anInterface');
+        expect(data.globals.theInterface).toContain('anInterface');
+        expect(data.globals.sameInterface).toContain('anInterface');
+        expect(data).toHaveProperty('jsNames');
+        expect(data.jsNames).toHaveProperty('functions');
+        expect(data.jsNames.functions).toHaveProperty('theInterface');
+        expect(data.jsNames.functions).not.toHaveProperty('sameInterface');
+        expect(data.jsNames.functions.theInterface).toContain('anInterface');
+      })
+      .catch(fail)
+      .then(done);
+  });
+});
