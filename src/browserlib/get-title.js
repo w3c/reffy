90cc9eb41c914db7d68ad86892ae7f6ad6134d23
François Daoust <fd@w3.org>
2020-03-14 17:23:50 +0100

    getTitle: drop exception to the rule for Web workers spec (#267)
    Fixes #256.

--- src/browserlib/get-title.js
+++ src/browserlib/get-title.js
@@ -3,13 +3,7 @@
  */
 export default function () {
   const title = window.document.querySelector('title');
-  if (window.location.href === 'https://html.spec.whatwg.org/multipage/workers.html') {
-    // Web Worker ED is a page of the HTML Living Standard.
-    // Report the appropriate title (crawler will still be confused because
-    // it won't find any normative references at the end of this page)
-    return 'Web Workers';
-  }
-  else if (title) {
+  if (title) {
     return title.textContent.trim();
   }
   else {
