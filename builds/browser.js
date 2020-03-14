2049d95822919a1cf06509059990b9515485ca85
François Daoust <fd@tidoust.net>
2020-03-14 17:08:43 +0100

    getTitle: drop exception to the rule for Web workers spec
    Fixes #256.

--- builds/browser.js
+++ builds/browser.js
@@ -7,13 +7,7 @@
    */
   function getTitle () {
     const title = window.document.querySelector('title');
-    if (window.location.href === 'https://html.spec.whatwg.org/multipage/workers.html') {
-      // Web Worker ED is a page of the HTML Living Standard.
-      // Report the appropriate title (crawler will still be confused because
-      // it won't find any normative references at the end of this page)
-      return 'Web Workers';
-    }
-    else if (title) {
+    if (title) {
       return title.textContent.trim();
     }
     else {
