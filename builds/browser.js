70e961915833fcd054dee7493a3a1ffcbce45273
Dominique Hazael-Massieux <dom@w3.org>
2020-11-26 11:30:28 +0100

    Ignore respec- prefixed ids
    They are not useful to our analysis, and they risk creating too much noise in extracts

--- builds/browser.js
+++ builds/browser.js
@@ -3301,7 +3301,9 @@ for more information.`;
       // Capture anchors set in <a name> when they're not dup of ids
       [...document.querySelectorAll('a[name]')]
         .filter(n => !n.id || n.id !== n.name).map(n => n.name)
-    );
+      // We ignore respec- prefixed ids to avoid keeping track of their evolution
+      // They're clearly not meant to be link target in any case
+    ).filter(id => !id.startsWith('respec-'));
   }
 
   /**
--- src/browserlib/extract-ids.js
+++ src/browserlib/extract-ids.js
@@ -6,5 +6,7 @@ export default function () {
     // Capture anchors set in <a name> when they're not dup of ids
     [...document.querySelectorAll('a[name]')]
       .filter(n => !n.id || n.id !== n.name).map(n => n.name)
-  );
+    // We ignore respec- prefixed ids to avoid keeping track of their evolution
+    // They're clearly not meant to be link target in any case
+  ).filter(id => !id.startsWith('respec-'));
 }
