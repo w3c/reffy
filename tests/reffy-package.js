283fc7198ad3c0931a76f8340ed9119637743cdc
François Daoust <fd@tidoust.net>
2021-02-10 16:57:59 +0100

    Add crawling tests using Reffy as a module
    This update adds a test similar to the tests in `crawl.js` that makes sure
    that the crawler runs successfully when Reffy is installed as a module.
    
    The test packs Reffy using `npm pack`, installs it to a tmp folder, and then
    runs the crawler in that folder.
    
    Common logic between `crawl.js` and the new test is now defined in a
    `nock-server.js` lib file.

---
+++ tests/reffy-package.js
@@ -0,0 +1,44 @@
+const { assert } = require("chai");
+const os = require("os");
+const fs = require("fs");
+const path = require("path");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const nock = require('../src/lib/nock-server');
+
+const specs = [
+  {url: "https://www.w3.org/TR/WOFF2/", nightly: {url: "https://w3c.github.io/woff/woff2/", pages:["https://w3c.github.io/woff/woff2/page.html"]}},
+  {url: "https://www.w3.org/TR/audio-output/", nightly: {url: "https://w3c.github.io/mediacapture-output/"}},
+  {url: "https://www.w3.org/TR/accelerometer/", nightly: {url: "https://w3c.github.io/accelerometer/"}}
+];
+
+describe("The npm package of Reffy", function () {
+  this.slow(30000);
+  this.timeout(60000);
+
+  let tmpdir;
+
+  before(async () => {
+    tmpdir = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'reffy-'));
+    const reffydir = path.resolve(__dirname, "..");
+    const { stdout: reffyPackage } = await exec(`npm pack "${reffydir}"`, { cwd: tmpdir});
+    await exec(`npm install "${reffyPackage}"`, { cwd: tmpdir });
+  });
+
+  it("can crawl specs", async () => {
+    const clidir = path.join(tmpdir, 'node_modules', 'reffy', 'src', 'cli');
+    const { crawlList } = require(path.join(clidir, 'crawl-specs'));
+    const refResults = JSON.parse(fs.readFileSync(__dirname + "/crawl-test.json", "utf-8"));
+    const results = await crawlList(specs);
+    // to avoid reporting bogus diff on updated date
+    results.forEach(s => delete s.date);
+    assert.deepEqual(refResults, results);
+    nock.isDone();
+  });
+
+  after(async () => {
+    if (tmpdir) {
+      await fs.promises.rmdir(tmpdir, { recursive: true });
+    }
+  });
+});
