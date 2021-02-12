5896c9b8cd440ae131b9a9ef5dffbf712114ccdf
François Daoust <fd@tidoust.net>
2021-02-12 10:32:14 +0100

    Create an index of IDL names
    The crawler now creates an `idlnames.json` file in the report folder, which is
    a list indexed by IDL names of pointers to the generated IDL fragments and
    parsed structures under `idlnames` and `idlnamesparsed`.
    
    Addresses https://github.com/w3c/webref/issues/89

--- src/cli/crawl-specs.js
+++ src/cli/crawl-specs.js
@@ -38,7 +38,7 @@ const completeWithAlternativeUrls = require('../lib/util').completeWithAlternati
 const isLatestLevelThatPasses = require('../lib/util').isLatestLevelThatPasses;
 const processSpecification = require('../lib/util').processSpecification;
 const { setupBrowser, teardownBrowser } = require('../lib/util');
-const { generateIdlNames, saveParsedIdlNames, saveIdlNames } = require('./generate-idlnames');
+const { generateIdlNames, saveIdlNames } = require('./generate-idlnames');
 
 /**
  * Flattens an array
@@ -283,9 +283,7 @@ async function saveResults(crawlOptions, data, folder) {
         ids: await getSubfolder('ids'),
         headings: await getSubfolder('headings'),
         idl: await getSubfolder('idl'),
-        idlnames: await getSubfolder('idlnames'),
         idlparsed: await getSubfolder('idlparsed'),
-        idlnamesparsed: await getSubfolder('idlnamesparsed'),
         links: await getSubfolder('links'),
         refs: await getSubfolder('refs')
     };
@@ -369,8 +367,7 @@ async function saveResults(crawlOptions, data, folder) {
 
     // Prepare and save IDL names exports
     const idlNames = generateIdlNames(data);
-    await saveParsedIdlNames(idlNames, folders.idlnamesparsed);
-    await saveIdlNames(idlNames, folders.idlnames);
+    await saveIdlNames(idlNames, folder);
 
     // Save IDL dumps for the latest level of a spec to the idl folder
     // TODO: the raw IDL of previous levels in a series is not saved anywhere.
