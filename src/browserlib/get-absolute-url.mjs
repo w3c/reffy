1c4f7a06c77f2a41d13b7f439439b6b81eeedc58
Francois Daoust <fd@tidoust.net>
2021-07-07 14:39:34 +0200

    Ignore ReSpec IDs that are not meant to be link targets
    IDs prefixed with `respec-` used to be ignored but that broke when the switch
    to absolute URLs was made. IDs prefixed with `dfn-panel-` can also be safely
    ignored.
    
    This update also adds tests for ID extraction, and fixes a bug with name
    extraction that these tests revealed.
    
    Fixes #668.

--- src/browserlib/get-absolute-url.mjs
+++ src/browserlib/get-absolute-url.mjs
@@ -4,14 +4,18 @@
  * @function
  * @public
  * @param {Element} node DOM node to look at. Must have an ID.
- * @param {Object} options singlePage asserts whether the spec is single page
+ * @param {Object} options "singlePage" asserts whether the spec is single page
  *   or whether that's unknown. Default is false for "unknown".
+ *   "attribute" tells function to use value of given attribute name instead of
+ *   the node's ID. Default is "id".
  * @return {String} Absolute URL ending with fragment ref
  */
-export default function (node, { singlePage } = { singlePage: false }) {
+export default function (node, { singlePage, attribute } =
+                               { singlePage: false, attribute: 'id' }) {
+  attribute = attribute ?? 'id';
   const page = singlePage ? null :
     node.closest('[data-reffy-page]')?.getAttribute('data-reffy-page');
   const url = new URL(page ?? window.location.href);
-  url.hash = '#' + node.id;
+  url.hash = '#' + node.getAttribute(attribute);
   return url.toString();
 }
