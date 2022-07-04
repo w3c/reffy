e06686e669a27494ce5d9951d09aab0365972f8d
Dominique Hazael-Massieux <dom@w3.org>
2022-07-04 09:49:47 +0200

    [events] Fix extraction of href from dfn (#998)

--- src/browserlib/extract-events.mjs
+++ src/browserlib/extract-events.mjs
@@ -190,7 +190,7 @@ export default function (spec) {
               // use the target of the link as our href
               event.href = eventEl.href;
             } else if (eventEl.tagName === "DFN" && eventEl.id) {
-              eventEl.href = href(eventEl);
+              event.href = href(eventEl);
             }
             event.targets = fromEventElementToTargetInterfaces(eventEl);
           }
