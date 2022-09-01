d32caaf8d258eaa1f36c8d8b3b63c8fe2012b2e4
Dominique Hazael-Massieux <dom@w3.org>
2022-09-01 17:20:11 +0200

    Restructure links extracts to keep original links
    Currently, reffy "canonicalize" urls before recording them in the links extracts.
    While that canonicalization is useful to regroup links that points to the same spec, it loses critical information (e.g. for links to multipage specs) and produces information that does not really reflect the reality of the spec, making it hard to produce reliable analysis.
    
    The patch instead records the original link untouched, and annotates it with the spec that can be identified after canonicalization.
    
    This means the structure of links extract will change.

--- src/browserlib/extract-links.mjs
+++ src/browserlib/extract-links.mjs
@@ -2,27 +2,39 @@ import { canonicalizeUrl } from './canonicalize-url.mjs';
 
 /**
  * Extract and canonicalize absolute links of the document and their fragments
- * FIXME: ⚠ Modify the DOM
 */
-export default function () {
-  // Ignore links from the "head" section, which either link to
-  // self, the GitHub repo, the implementation report, and other
-  // documents that don't need to appear in the list of references.
+export default function (spec, _, specs) {
   const links = {};
-  [...document.querySelectorAll('.head a[href]')].forEach(n => n.href = '');
   document.querySelectorAll('a[href^=http]').forEach(n => {
-    const url = canonicalizeUrl(n.href);
-    if (!links[url]) {
-      links[url] = new Set();
+    // Ignore links from the "head" section, which either link to
+    // self, the GitHub repo, the implementation report, and other
+    // documents that don't need to appear in the list of references.
+    if (n.closest('.head')) return;
+    const pageUrl = n.href.split('#')[0];
+    if (!links[pageUrl]) {
+      links[pageUrl] = {anchors: new Set()};
     }
     if (n.href.includes('#') && n.href.split('#')[1]) {
-      links[url].add(n.href.split('#')[1]);
+      links[pageUrl].anchors.add(n.href.split('#')[1]);
+    }
+
+    // Annotate with the spec to which the page belong if we can find one
+    if (Array.isArray(specs)) {
+      const specUrl = canonicalizeUrl(pageUrl);
+      let matchingSpec = specs.find(s => s?.release?.url === specUrl || s?.nightly?.url === specUrl || s?.series?.nightlyUrl === specUrl || s?.series?.releaseUrl === specUrl || s?.nightly?.pages?.includes(specUrl) || s?.release?.pages?.includes(specUrl));
+      if (matchingSpec) {
+	links[pageUrl].specShortname = matchingSpec.shortname;
+      }
     }
   });
   return Object.keys(links)
+    .sort()
   // turning sets into arrays
     .reduce((acc, u) => {
-      acc[u] = [...links[u]];
+      acc[u] = {specShortname: links[u].specShortname};
+      if (links[u].anchors.size > 0) {
+	acc[u].anchors = [...links[u].anchors];
+      }
       return acc;
   }, {});
 }
