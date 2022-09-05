f3e68e0e027c025223deabc847af90d2badab214
Dominique Hazael-Massieux <dom@w3.org>
2022-09-05 11:47:13 +0200

    Simplify canonicalizeUrl code
    More complex options are only needed in strudy, which will maintain its own version for now
    See also https://github.com/w3c/reffy/pull/1055#discussion_r961818285

--- src/postprocessing/annotate-links.js
+++ src/postprocessing/annotate-links.js
@@ -5,10 +5,8 @@
 const fs = require('fs');
 const path = require('path');
 
-function canonicalizeUrl(url, options) {
-    options = options || {};
-
-    let canon = url.replace(/^http:/, 'https:')
+function canonicalizeUrl(url) {
+    return url.replace(/^http:/, 'https:')
             .split('#')[0]
             .replace('index.html', '')
             .replace('Overview.html', '')
@@ -18,20 +16,6 @@ function canonicalizeUrl(url, options) {
             .replace(/w3.org\/TR\/([^\/]+)$/, 'w3.org/TR/$1/') // enforce trailing slash
             .replace(/w3c.github.io\/([^\/]+)$/, 'w3c.github.io/$1/') // enforce trailing slash for ED on GitHub
         ;
-
-    if (options.datedToLatest) {
-        canon = canon.replace(
-            /w3.org\/TR\/[0-9]{4}\/[A-Z]+-(.*)-[0-9]{8}\/?/,
-            'w3.org/TR/$1/');
-    }
-
-    let equivalentUrls = (options.equivalents) ? options.equivalents[canon] : null;
-    if (Array.isArray(equivalentUrls)) {
-        return (options.returnAlternatives ? equivalentUrls : equivalentUrls[0]);
-    }
-    else {
-        return (equivalentUrls ? equivalentUrls : canon);
-    }
 }
 
 const needsSaving = {};
