adb73f680c3b095f6abef26fa3ca4b4cf67543a7
Francois Daoust <fd@tidoust.net>
2024-12-07 22:59:28 +0100

    Extract CDDL definitions
    Needed for https://github.com/w3c/webref/issues/1353.
    
    With this update, Reffy now looks for and extracts CDDL content defined in
    `<pre class="cddl">` block. The logic is vastly similar to the logic used for
    IDL. Shared code was factored out accordingly.
    
    Something specific about CDDL: on top of generating text extracts, the goal is
    also to create one extract per CDDL module that the spec defines. To associate
    a `<pre>` block with one or more CDDL module, the code looks for a possible
    `data-cddl-module` module, or for module names in the `class` attribute
    (prefixed by `cddl-` or suffixed by `-cddl`). The former isn't used by any spec
    but is the envisioned mechanism in Bikeshed to define the association, the
    latter is the convention currently used in the WebDriver BiDi specification.
    
    When a spec defines modules, CDDL defined in a `<pre>` block with no explicit
    module annotation is considered to be defined for all modules (not doing so
    would essentially mean the CDDL would not be defined for any module, which
    seems weird).
    
    When there is CDDL, the extraction produces:
    1. an extract that contains all CDDL definitions: `cddl/[shortname].cddl`
    2. one extract per CDDL module: `cddl/[shortname]-[modulename].cddl`
    (I'm going to assume that no one is ever going to define a module name that
    would make `[shortname]-[modulename]` collide with the shortname of another
    spec).
    
    Note: some specs that define CDDL do not flag the `<pre>` blocks in any way
    (Open Screen Protocol, WebAuthn). Extraction won't work for them for now. Also,
    there are a couple of places in the WebDriver BiDi spec that use a
    `<pre class="cddl">` block to *reference* a CDDL construct defined elsewhere.
    Extraction will happily include these references as well, leading to CDDL
    extracts that contain invalid CDDL. These need fixing in the specs.

---
+++ tests/extract-cddl.js
@@ -0,0 +1,183 @@
+import assert from 'node:assert';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import puppeteer from 'puppeteer';
+import { rollup } from 'rollup';
+const scriptPath = path.dirname(fileURLToPath(import.meta.url));
+
+const tests = [
+  {
+    title: 'extracts CDDL from pre.cddl',
+    html: `<pre class="cddl">cddl = tstr</pre>`,
+    res: 'cddl = tstr'
+  },
+
+  {
+    title: 'produces no CDDL when there is no CDDL',
+    html: `<p>Me no define CDDL</p>`,
+    res: []
+  },
+
+  {
+    title: 'merges multiples blocks of CDDL',
+    html: `<pre class="cddl">cddl = * rule</pre>
+           <pre class="cddl">rule = tstr</pre>`,
+    res: `cddl = * rule
+
+rule = tstr`
+  },
+
+  {
+    title: 'strips trailing spaces',
+    html: `<pre class="cddl">
+            cddl = * rule    </pre>`,
+    res: `cddl = * rule`
+  },
+
+  {
+    title: 'preserves internal indentation',
+    html: `<pre class="cddl">
+            rule = (
+              typedef /
+              groupdef
+            )
+            typedef = tstr
+              groupdef = tstr
+          </pre>`,
+    res: `rule = (
+  typedef /
+  groupdef
+)
+typedef = tstr
+  groupdef = tstr`
+  },
+
+  {
+    title: 'extracts CDDL module names from data-cddl-module',
+    html: `<pre class="cddl" data-cddl-module="mod">cddl = tstr</pre>`,
+    res: [
+      { name: '',    cddl: 'cddl = tstr' },
+      { name: 'mod', cddl: 'cddl = tstr' }
+    ]
+  },
+
+  {
+    title: 'extracts CDDL module name defined as class',
+    html: `<pre class="cddl mod1-cddl cddl-mod2">cddl = tstr</pre>`,
+    res: [
+      { name: '',     cddl: 'cddl = tstr' },
+      { name: 'mod1', cddl: 'cddl = tstr' },
+      { name: 'mod2', cddl: 'cddl = tstr' }
+    ]
+  },
+
+  {
+    title: 'assembles CDDL in modules',
+    html: `
+      <pre class="cddl">
+        cddl = * rule
+      </pre>
+      <pre class="cddl" data-cddl-module="mod1, mod2">
+        rule = (typedef / groupdef)
+      </pre>
+      <pre class="cddl" data-cddl-module="mod1">
+        cddl1 = tstr
+      </pre>
+      <pre class="cddl" data-cddl-module="mod2">
+        cddl2 = tstr
+      </pre>
+      <pre class="cddl">
+        typedef = tstr
+        groupdef = tstr
+      </pre>
+    `,
+    res: [
+      {
+        name: '',
+        cddl:
+`cddl = * rule
+
+rule = (typedef / groupdef)
+
+cddl1 = tstr
+
+cddl2 = tstr
+
+typedef = tstr
+groupdef = tstr`
+      },
+      {
+        name: 'mod1',
+        cddl:
+`cddl = * rule
+
+rule = (typedef / groupdef)
+
+cddl1 = tstr
+
+typedef = tstr
+groupdef = tstr`
+      },
+      {
+        name: 'mod2',
+        cddl:
+`cddl = * rule
+
+rule = (typedef / groupdef)
+
+cddl2 = tstr
+
+typedef = tstr
+groupdef = tstr`
+      }
+    ]
+  }
+];
+
+function isString(x) {
+  return Object.prototype.toString.call(x) === "[object String]";
+}
+
+describe("CDDL extraction", function () {
+  this.slow(5000);
+
+  let browser;
+  let extractCode;
+
+  before(async () => {
+    const extractBundle = await rollup({
+      input: path.resolve(scriptPath, '../src/browserlib/extract-cddl.mjs')
+    });
+    const extractOutput = (await extractBundle.generate({
+      name: 'extractCddl',
+      format: 'iife'
+    })).output;
+    extractCode = extractOutput[0].code;
+
+    browser = await puppeteer.launch({ headless: true });
+  });
+
+  for (const test of tests) {
+    it(test.title, async () => {
+      const page = await browser.newPage();
+      page.setContent(test.html);
+      await page.addScriptTag({ content: extractCode });
+
+      const extracted = await page.evaluate(async () => extractCddl());
+      await page.close();
+
+      if (isString(test.res)) {
+        assert.deepEqual(extracted.length, 1,
+          `Expected extraction to return 1 CDDL module, got ${extracted.length}`);
+        assert.deepEqual(extracted[0].cddl, test.res);
+      }
+      else {
+        assert.deepEqual(extracted, test.res);
+      }
+    });
+  }
+
+  after(async () => {
+    await browser.close();
+  });
+});
