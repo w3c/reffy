87b96181b97082f0d801184e82e6c7ff62459cb9
Francois Daoust <fd@tidoust.net>
2025-04-11 16:44:48 +0200

    Add `--markdown` and `--summary` crawl options
    The `--summary` crawl option tells Reffy to attach a crawl summary in Markdown
    to each spec result. The summary is added to a `crawlSummary` property.
    
    The `--markdown` crawl option tells Reffy to output a Markdown report instead
    of the regular JSON output.
    
    The Markdown summary is fairly basic for now. It starts with a short summary,
    then details interesting things that got extracted (CSS terms, exported terms,
    Web IDL), along with links to the online xref database search where
    appropriate. The details are reported in expandable sections.
    
    The summary is very likely to evolve:
    - to get additional details on other extracts (such as CDDL or headings)
    - to improve the ordering and formatting (for example, details on exported
    terms could probably omit IDL terms that are "for" something since that's less
    likely to be a nuisance, and could probably omit IDL terms altogether as main
    terms get reported in the IDL details)
    
    The options will allow us to report on crawl results in browser-specs issues
    when a new spec gets proposed.

---
+++ tests/markdown-report.js
@@ -0,0 +1,137 @@
+import assert from 'node:assert';
+import { generateSpecReport } from '../src/lib/markdown-report.js';
+import crawlTest from './crawl-test.json' with { type: 'json' };
+
+describe('The markdown report', () => {
+  it('produces a suitable summary', async () => {
+    const specResult = crawlTest[0];
+    assert.strictEqual(await generateSpecReport(crawlTest[0]), `Crawl summary:
+- Title: WOFF2
+- Canonical URL: [https://www.w3.org/TR/WOFF2/](https://www.w3.org/TR/WOFF2/)
+- Crawled URL: [https://w3c.github.io/woff/woff2/](https://w3c.github.io/woff/woff2/)
+- Spec defines:
+  - Terms: 1 private
+  - Headings: 1 found
+  - IDs: 2 found
+- No Algorithms, CDDL, CSS, Events, Links, References, Web IDL definitions found`);
+  });
+
+
+  it('produces CSS details', async () => {
+    const specResult = Object.assign({}, crawlTest[0], {
+      css: {
+        atrules: [
+          {
+            name: '@charset',
+            href: 'https://drafts.csswg.org/css-syntax-3/#at-ruledef-charset'
+          }
+        ],
+        properties: [
+          {
+            name: 'block-step-size',
+            href: 'https://drafts.csswg.org/css-rhythm-1/#propdef-block-step-size',
+            value: 'none | <length [0,∞]>',
+            initial: 'none'
+          }
+        ],
+        selectors: [
+          {
+            name: ':host',
+            href: 'https://drafts.csswg.org/css-scoping-1/#selectordef-host'
+          }
+        ]
+      }
+    });
+    assert.strictEqual(await generateSpecReport(specResult), `Crawl summary:
+- Title: WOFF2
+- Canonical URL: [https://www.w3.org/TR/WOFF2/](https://www.w3.org/TR/WOFF2/)
+- Crawled URL: [https://w3c.github.io/woff/woff2/](https://w3c.github.io/woff/woff2/)
+- Spec defines:
+  - CSS: 1 property, 1 at-rule, 1 selector
+  - Terms: 1 private
+  - Headings: 1 found
+  - IDs: 2 found
+- No Algorithms, CDDL, Events, Links, References, Web IDL definitions found
+
+<details>
+<summary>1 CSS property</summary>
+
+- [\`block-step-size\`](https://drafts.csswg.org/css-rhythm-1/#propdef-block-step-size) ([xref search](https://respec.org/xref/?term=block-step-size&types=css-at-rule%2Ccss-descriptor%2Ccss-function%2Ccss-property%2Ccss-selector%2Ccss-type%2Ccss-value))
+</details>
+<details>
+<summary>1 CSS at-rule</summary>
+
+- [\`@charset\`](https://drafts.csswg.org/css-syntax-3/#at-ruledef-charset) ([xref search](https://respec.org/xref/?term=%40charset&types=css-at-rule%2Ccss-descriptor%2Ccss-function%2Ccss-property%2Ccss-selector%2Ccss-type%2Ccss-value))
+</details>
+<details>
+<summary>1 CSS selector</summary>
+
+- [\`:host\`](https://drafts.csswg.org/css-scoping-1/#selectordef-host) ([xref search](https://respec.org/xref/?term=%3Ahost&types=css-at-rule%2Ccss-descriptor%2Ccss-function%2Ccss-property%2Ccss-selector%2Ccss-type%2Ccss-value))
+</details>`);
+  });
+
+
+  it('produces dfns details', async () => {
+    const dfn1 = Object.assign({}, crawlTest[0].dfns[0], { access: 'public' });
+    const dfn2 = Object.assign({}, dfn1, { type: 'attribute', for: ['Bar'] });
+    const specResult = Object.assign({}, crawlTest[0], { dfns: [dfn1, dfn2] });
+    assert.strictEqual(await generateSpecReport(specResult), `Crawl summary:
+- Title: WOFF2
+- Canonical URL: [https://www.w3.org/TR/WOFF2/](https://www.w3.org/TR/WOFF2/)
+- Crawled URL: [https://w3c.github.io/woff/woff2/](https://w3c.github.io/woff/woff2/)
+- Spec defines:
+  - Terms: 2 public
+  - Headings: 1 found
+  - IDs: 2 found
+- No Algorithms, CDDL, CSS, Events, Links, References, Web IDL definitions found
+
+<details>
+<summary>2 exported terms</summary>
+
+- [Foo](https://w3c.github.io/woff/woff2/#foo), type dfn ([xref search](https://respec.org/xref/?term=Foo))
+- [\`Foo\`](https://w3c.github.io/woff/woff2/#foo) for \`Bar\`, type attribute ([xref search](https://respec.org/xref/?term=Foo))
+</details>`);
+  });
+
+
+  it('produces IDL details', async () => {
+    const specResult = Object.assign({}, crawlTest[0], {
+      idl: `
+[SecureContext, Exposed=Window]
+interface Presentation {
+};
+
+partial interface Presentation {
+  attribute PresentationRequest? defaultRequest;
+};
+
+partial interface Presentation {
+  readonly attribute PresentationReceiver? receiver;
+};
+
+dictionary PresentationConnectionAvailableEventInit : EventInit {
+  required PresentationConnection connection;
+};
+
+enum PresentationConnectionState { "connecting", "connected", "closed", "terminated" };
+  ` });
+    assert.strictEqual(await generateSpecReport(specResult), `Crawl summary:
+- Title: WOFF2
+- Canonical URL: [https://www.w3.org/TR/WOFF2/](https://www.w3.org/TR/WOFF2/)
+- Crawled URL: [https://w3c.github.io/woff/woff2/](https://w3c.github.io/woff/woff2/)
+- Spec defines:
+  - Web IDL: 3 names (or partials)
+  - Terms: 1 private
+  - Headings: 1 found
+  - IDs: 2 found
+- No Algorithms, CDDL, CSS, Events, Links, References definitions found
+
+<details>
+<summary>3 Web IDL names</summary>
+
+- interface \`Presentation\` ([xref search](https://respec.org/xref/?term=Presentation&types=_IDL_))
+- dictionary \`PresentationConnectionAvailableEventInit\` ([xref search](https://respec.org/xref/?term=PresentationConnectionAvailableEventInit&types=_IDL_))
+- enum \`PresentationConnectionState\` ([xref search](https://respec.org/xref/?term=PresentationConnectionState&types=_IDL_))
+</details>`);
+  });
+});
