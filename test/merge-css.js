45222b019437b43e69a9b3553cde67d606dc513c
François Daoust <fd@w3.org>
2025-06-04 14:11:31 +0200

    Add cssmerge post-processor to consolidate CSS extracts (#1849)
    This adds a cssmerge post-processor at the crawl level that consolidates
    definitions from CSS extracts into a single file. The CSS entries in the
    resulting file are de-duplicated. The module starts with a number of comments
    that detail the approach.
    
    The intent is to create a view similar to that offered in MDN data to address
    https://github.com/w3c/webref/issues/1519
    
    Comments at the top of the module also detail the main differences between the
    two views. Main one is structural: we tend to use arrays in Webref so this code
    also produces arrays, while MDN data uses indexed objects. It's easy to build
    an indexed view from an array though.
    
    Consolidation focuses on syntaxes and the code does not attempt to compute the
    `units` list that exists in MDN data. It is easy to build that list by looking
    at values of a handful of CSS properties. We could add it later on to the
    consolidated file if that proves needed.
    
    A couple of remaining open questions:
    1. This creates a `css.json` file, similar to the `events.json` file that we
    create when we post-process events. That's nice but it would be good to ship
    that file in `@webref/css` in the end. That package already has a `CSS.json`
    file at the root. How/Where to package the `css.json` file?
    2. To de-duplicate, the most recent definition is used. That's the only
    approach I can think of that can be automated. Consumers of MDN data may need
    a more nuanced approach where the syntax better matches what core browsers
    currently support. We can perhaps refine the process later on if needed with
    some sort of patching mechanism (but my hunch is that this will remain a manual
    process).

---
+++ test/merge-css.js
@@ -0,0 +1,507 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import cssmerge from '../src/postprocessing/cssmerge.js';
+import crawlTest from './crawl-test.json' with { type: 'json' };
+
+const emptyExtract = {
+  atrules: [],
+  properties: [],
+  selectors: [],
+  values: []
+};
+
+const emptyMerged = {
+  atrules: [],
+  functions: [],
+  properties: [],
+  selectors: [],
+  types: []
+};
+
+const atrule1 = {
+  name: '@-webkit-keyframes',
+  href: 'https://compat.spec.whatwg.org/#at-ruledef--webkit-keyframes',
+  descriptors: []
+};
+
+const atrule2 = {
+  name: '@media',
+  href: 'https://drafts.csswg.org/css-conditional-3/#at-ruledef-media',
+  descriptors: []
+};
+
+const descriptor1 = {
+  name: 'descriptor1',
+  href: 'https://compat.spec.whatwg.org/#descdef-media--webkit-device-pixel-ratio',
+  for: '@media',
+  value: '<number>',
+  type: 'range'
+};
+
+const descriptor2 = {
+  name: 'descriptor2',
+  href: 'https://compat.spec.whatwg.org/#descdef-media--webkit-transform-3d',
+  for: '@media',
+  value: '<mq-boolean>',
+  type: 'discrete'
+};
+
+const property1 = {
+  name: 'overlay',
+  href: 'https://drafts.csswg.org/css-position-4/#propdef-overlay',
+  value: 'none | auto',
+  initial: 'none',
+  appliesTo: 'all elements',
+  inherited: 'no',
+  percentages: 'n/a',
+  computedValue: 'as specified',
+  canonicalOrder: 'per grammar',
+  animationType: 'see prose',
+  styleDeclaration: [
+    'overlay'
+  ]
+};
+
+const propertyLegacy = {
+  name: 'good-old-overlay',
+  legacyAliasOf: 'overlay'
+};
+
+const selector1 = {
+  name: ':first-of-page',
+  href: 'https://drafts.csswg.org/css-gcpm-4/#selectordef-first-of-page',
+  value: ':first-of-page'
+};
+
+const type1 = {
+  name: '<repetition>',
+  href: 'https://drafts.csswg.org/css-backgrounds-4/#typedef-repetition',
+  type: 'type',
+  value: 'repeat | space | round | no-repeat'
+};
+
+const functionVar = {
+  name: 'var()',
+  href: 'https://drafts.csswg.org/css-variables-2/#funcdef-var',
+  type: 'function',
+  value: 'var( <custom-property-name> , <declaration-value>? )'
+};
+
+const functionEnv = {
+  name: 'env()',
+  href: 'https://drafts.csswg.org/css-env-1/#funcdef-env',
+  type: 'function',
+  value: 'env( <custom-ident> <integer [0,∞]>*, <declaration-value>? )'
+};
+
+
+// Note: Post-processing steps tend to update data in place. That's totally
+// fine when they are run as post-processing steps within an actual Reffy crawl
+// because Reffy sends a copy of the crawl data to the post-processing module.
+// That's problematic for tests because that means the test constructs defined
+// above and used across tests may be changed in place. Hence the calls to
+// `structuredClone` to force tests to run on copies of test constructs.
+describe('CSS extracts consolidation', function () {
+  it('generates an empty report when specs do not define CSS', async () => {
+    const result = await cssmerge.run({ results: crawlTest });
+    assert.deepEqual(result, emptyMerged);
+  });
+
+
+  it('includes all definitions and sorts them', async () => {
+    const results = structuredClone([
+      {
+        shortname: 'css-stuff-1',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          atrules: [
+            atrule1,
+            Object.assign({}, atrule2, {
+              descriptors: [descriptor1]
+            })
+          ],
+          selectors: [selector1],
+          values: [
+            type1,
+            functionVar
+          ]
+        })
+      },
+      {
+        shortname: 'css-otherstuff-1',
+        series: { shortname: 'css-otherstuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          atrules: [
+            Object.assign({}, atrule2, {
+              value: '@media <media-query-list> { <rule-list> }',
+              descriptors: [descriptor2]
+            })
+          ],
+          properties: [property1],
+          values: [functionEnv]
+        })
+      }
+    ]);
+    const result = await cssmerge.run({ results });
+    const expected = {
+      atrules: [
+        atrule1,
+        Object.assign({}, atrule2, {
+          value: '@media <media-query-list> { <rule-list> }',
+          descriptors: [descriptor1, descriptor2]
+        })
+      ],
+      // Note: env() before var() if code sorts things correctly
+      functions: [functionEnv, functionVar],
+      properties: [property1],
+      selectors: [selector1],
+      types: [type1]
+    };
+    // Note: comparing per category to ease identification of problems if
+    // test fails for some reason!
+
+    for (const category of Object.keys(expected)) {
+      assert.deepEqual(result[category], expected[category]);
+    }
+  });
+
+
+  it('includes nested functions and types', async () => {
+    const results = structuredClone([
+      {
+        shortname: 'css-stuff-1',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          values: [
+            {
+              name: '<track-repeat>',
+              href: 'https://drafts.csswg.org/css-grid-2/#typedef-track-repeat',
+              type: 'type',
+              values: [
+                functionEnv,
+                type1
+              ]
+            }
+          ]
+        })
+      }
+    ]);
+    const result = await cssmerge.run({ results });
+    assert.deepEqual(result, Object.assign({}, emptyMerged, {
+      functions: [
+        Object.assign({}, functionEnv, {
+          for: '<track-repeat>'
+        })
+      ],
+      types: [
+        Object.assign({}, type1, {
+          for: '<track-repeat>'
+        }),
+        {
+          name: '<track-repeat>',
+          href: 'https://drafts.csswg.org/css-grid-2/#typedef-track-repeat',
+          type: 'type'
+        }
+      ]
+    }));
+  });
+
+
+  it('reports the most recent definition in a series', async () => {
+    const results = structuredClone([
+      {
+        shortname: 'css-stuff-1',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          properties: [
+            property1
+          ]
+        })
+      },
+      {
+        shortname: 'css-stuff-2',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '2',
+        css: Object.assign({}, emptyExtract, {
+          properties: [
+            Object.assign({}, property1, {
+              value: 'none | auto | train'
+            })
+          ]
+        })
+      }
+    ]);
+    const result = await cssmerge.run({ results });
+    assert.deepEqual(result.properties, [
+      Object.assign({}, property1, {
+        value: 'none | auto | train'
+      })
+    ]);
+  });
+
+
+  it('merges extended property definitions', async () => {
+    const results = structuredClone([
+      {
+        shortname: 'css-stuff-1',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          properties: [
+            property1
+          ]
+        })
+      },
+      {
+        shortname: 'css-otherstuff-2',
+        series: { shortname: 'css-otherstuff' },
+        seriesVersion: '2',
+        css: Object.assign({}, emptyExtract, {
+          properties: [
+            Object.assign({}, property1, {
+              value: null,
+              newValues: 'train'
+            })
+          ]
+        })
+      }
+    ]);
+    const result = await cssmerge.run({ results });
+    assert.deepEqual(result.properties, [
+      Object.assign({}, property1, {
+        value: 'none | auto | train'
+      })
+    ]);
+  });
+
+
+  it('discards duplicated property extension definitions', async () => {
+    const results = structuredClone([
+      {
+        shortname: 'css-stuff-1',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          properties: [
+            property1
+          ]
+        })
+      },
+      {
+        shortname: 'css-otherstuff-1',
+        series: { shortname: 'css-otherstuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          properties: [
+            Object.assign({}, property1, {
+              value: null,
+              newValues: 'train'
+            })
+          ]
+        })
+      },
+      {
+        shortname: 'css-otherstuff-2',
+        series: { shortname: 'css-otherstuff' },
+        seriesVersion: '2',
+        css: Object.assign({}, emptyExtract, {
+          properties: [
+            Object.assign({}, property1, {
+              value: null,
+              newValues: 'train'
+            })
+          ]
+        })
+      }
+    ]);
+    const result = await cssmerge.run({ results });
+    assert.deepEqual(result.properties, [
+      Object.assign({}, property1, {
+        value: 'none | auto | train'
+      })
+    ]);
+  });
+
+
+  it('merges at-rules descriptors definitions', async () => {
+    const results = structuredClone([
+      {
+        shortname: 'css-stuff-1',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          atrules: [
+            Object.assign({}, atrule2, {
+              value: '@media foo',
+              descriptors: [descriptor1]
+            })
+          ]
+        })
+      },
+      {
+        shortname: 'css-otherstuff-1',
+        series: { shortname: 'css-otherstuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          atrules: [
+            Object.assign({}, atrule2, {
+              descriptors: [descriptor2]
+            })
+          ]
+        })
+      }
+    ]);
+    const result = await cssmerge.run({ results });
+    assert.deepEqual(result.atrules, [
+      Object.assign({}, atrule2, {
+        value: '@media foo',
+        descriptors: [descriptor1, descriptor2]
+      })
+    ]);
+  });
+
+
+  it('discards duplicated descriptor definitions', async () => {
+    const results = structuredClone([
+      {
+        shortname: 'css-stuff-1',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          atrules: [
+            Object.assign({}, atrule2, {
+              value: '@media foo',
+              descriptors: [descriptor1]
+            })
+          ]
+        })
+      },
+      {
+        shortname: 'css-otherstuff-1',
+        series: { shortname: 'css-otherstuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          atrules: [
+            Object.assign({}, atrule2, {
+              descriptors: [descriptor2]
+            })
+          ]
+        })
+      },
+      {
+        shortname: 'css-otherstuff-2',
+        series: { shortname: 'css-otherstuff' },
+        seriesVersion: '2',
+        css: Object.assign({}, emptyExtract, {
+          atrules: [
+            Object.assign({}, atrule2, {
+              descriptors: [descriptor2]
+            })
+          ]
+        })
+      }
+    ]);
+    const result = await cssmerge.run({ results });
+    assert.deepEqual(result.atrules, [
+      Object.assign({}, atrule2, {
+        value: '@media foo',
+        descriptors: [descriptor1, descriptor2]
+      })
+    ]);
+  });
+
+
+  it('discards scoped definitions that match an unscoped one', async () => {
+    const results = structuredClone([
+      {
+        shortname: 'css-stuff-1',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          values: [
+            Object.assign({}, type1, {
+              values: [
+                functionEnv
+              ]
+            }),
+            functionEnv
+          ]
+        })
+      }
+    ]);
+    const result = await cssmerge.run({ results });
+    assert.deepEqual(result, Object.assign({}, emptyMerged, {
+      functions: [functionEnv],
+      types: [type1]
+    }));
+  });
+
+
+  it('expands at-rules syntaxes when possible', async () => {
+    const results = structuredClone([
+      {
+        shortname: 'css-stuff-1',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          atrules: [
+            Object.assign({}, atrule2, {
+              value: '@media { <declaration-list> }',
+              descriptors: [descriptor1]
+            })
+          ]
+        })
+      },
+      {
+        shortname: 'css-otherstuff-1',
+        series: { shortname: 'css-otherstuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          atrules: [
+            Object.assign({}, atrule2, {
+              descriptors: [descriptor2]
+            })
+          ]
+        })
+      }
+    ]);
+    const result = await cssmerge.run({ results });
+    assert.deepEqual(result.atrules[0].value, `@media {
+  [ descriptor1: [ <number> ]; ] ||
+  [ descriptor2: [ <mq-boolean> ]; ]
+}`);
+  });
+
+
+  it('sets the syntax of legacy aliases', async () => {
+    const results = structuredClone([
+      {
+        shortname: 'css-stuff-1',
+        series: { shortname: 'css-stuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          properties: [propertyLegacy]
+        })
+      },
+      {
+        shortname: 'css-otherstuff-1',
+        series: { shortname: 'css-otherstuff' },
+        seriesVersion: '1',
+        css: Object.assign({}, emptyExtract, {
+          properties: [property1]
+        })
+      }
+    ]);
+    const result = await cssmerge.run({ results });
+    assert.deepEqual(result.properties, [
+      Object.assign({}, propertyLegacy, {
+        value: property1.value
+      }),
+      property1
+    ]);
+  });
+});
